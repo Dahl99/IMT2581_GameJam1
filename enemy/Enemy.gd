3d931f4deac78be6f7bd058430f7420b2a17db46
Daniel Dahl <danieldahl99@gmail.com>
2021-02-06 20:53:49 +0100

    Made cliff checking for enemies individual

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -2,15 +2,17 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 export var direction = -1
+export var detects_cliffs = true
 
 func _ready():
 	if direction == -1:
 		$AnimatedSprite.flip_h = true
 
 	$RayCast2D.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	$RayCast2D.enabled = detects_cliffs
 
 func _physics_process(_delta):
-	if is_on_wall() || !$RayCast2D.is_colliding():
+	if is_on_wall() || !$RayCast2D.is_colliding() && detects_cliffs && is_on_floor():
 		direction *= -1
 		$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
 		$RayCast2D.position.x = $CollisionShape2D.shape.get_extents().x * direction
